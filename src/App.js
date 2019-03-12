a2841ac1930550e70835082250a385d3088433cb
Isaac Moore <icmoore1@bellsouth.net>
2019-03-12 10:20:08 -0400

    added photos and info to clicked flags.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,30 @@ class App extends Component {
      };
    }
 
+   closeAllMarkers = () => {
+       const markers = this.state.markers.map(marker => {
+         marker.isOpen = false;
+         return marker;
+       });
+       this.setState({ markers: Object.assign(this.state.markers, markers) });
+     };
+
+   handleMarkerClick = (marker) => {
+      this.closeAllMarkers();
+      //console.log(marker);
+      marker.isOpen = true;
+      this.setState({ markers: Object.assign(this.state.markers, marker) });
+
+      const venue = this.state.venues.find(venue => venue.id === marker.id);
+    //  console.log(venue, "here 35");
+    //  SquareAPI.getVenueDetails(marker.id).then(res => console.log(res));
+    SquareAPI.getVenueDetails(marker.id).then(res => {
+        const newVenue = Object.assign(venue, res.response.venue);
+        this.setState({ venues: Object.assign(this.state.venues, newVenue) });
+        console.log(newVenue);
+      });
+  };
+
   componentDidMount(){
     SquareAPI.search({
       near:"Austin,TX",
@@ -32,7 +56,7 @@ class App extends Component {
             lng: venue.location.lng,
             isOpen: false,
             isVisible: true,
-          //  id: venue.id
+            id: venue.id
           };
         });
         this.setState({ venues, markers, center });
@@ -43,8 +67,8 @@ class App extends Component {
     return (
       <div className="App">
       //  <Map/>
-      //  <Map {...this.state} handleMarkerClick={this.handleMarkerClick} />
-      <Map {...this.state} />
+      <Map {...this.state} handleMarkerClick={this.handleMarkerClick} />
+    //  <Map {...this.state} />
       </div>
     );
   }
--- src/component/Map.js
+++ src/component/Map.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-import {withScriptjs,withGoogleMap,GoogleMap,Marker} from 'react-google-maps';
+import {withScriptjs,withGoogleMap,GoogleMap,Marker,InfoWindow} from 'react-google-maps';
 
 const MyMapComponent = withScriptjs(
    withGoogleMap(props => (
@@ -11,12 +11,31 @@ const MyMapComponent = withScriptjs(
       center={props.center}
       >
     {props.markers &&
-       props.markers
-         .filter(marker => marker.isVisible)
-         .map((marker, index) => (
-           <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
-      ))}
-      </GoogleMap>
+       props.markers.filter(marker => marker.isVisible).map((marker, index) => {
+         const venueInfo = props.venues.find(venue => venue.id === marker.id);
+         return (
+           <Marker
+            key={index}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => props.handleMarkerClick(marker)}
+            >
+            {marker.isOpen &&  venueInfo.bestPhoto && (
+              <InfoWindow>
+                <React.Fragment>
+                <img
+                  src={`${venueInfo.bestPhoto.prefix}200x200${
+                    venueInfo.bestPhoto.suffix
+                   }`}
+                   alt={`${venueInfo.name}`}
+                        />
+              <p>{venueInfo.name}</p>
+                </React.Fragment>
+                    </InfoWindow>
+                )}
+                </Marker>
+                );
+              })}
+        </GoogleMap>
     ))
   );
 
