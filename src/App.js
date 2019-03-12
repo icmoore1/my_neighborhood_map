07b98a603f4c4ebc3367b48bb7d4c842afcfa35b
Isaac Moore <icmoore1@bellsouth.net>
2019-03-11 21:29:23 -0400

    added markers but positioning is jacked up.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,66 @@ import './App.css';
 import Map from './component/Map';
 
 class App extends Component {
+  constructor() {
+     super();
+     this.state = {
+       venues: [],
+       markers: [],
+       center: [],
+       zoom: 12,
+    //   updateSuperState: obj => {
+    //     this.setState(obj);
+    //   }
+     };
+   }
+
   componentDidMount(){
     SquareAPI.search({
       near:"Austin,TX",
       query:"tacos",
       limit: 10
-    }).then(results => console.log(results));
-  }
+    }).then(results => {
+      //console.log(results));
+      const { venues } = results.response;
+        const { center } = results.response.geocode.feature.geometry;
+        const markers = venues.map(venue => {
+          return {
+            lat: venue.location.lat,
+            lng: venue.location.lng,
+            isOpen: false,
+            isVisible: true,
+          //  id: venue.id
+          };
+        });
+        this.setState({ venues, markers, center });
+      });
+    }
+
   render() {
     return (
       <div className="App">
-        <Map/>
+      //  <Map/>
+      //  <Map {...this.state} handleMarkerClick={this.handleMarkerClick} />
+      <Map {...this.state} />
       </div>
     );
   }
 }
+
+//componentDidMount(){
+//  SquareAPI.search({
+//    near:"Austin,TX",
+//    query:"tacos",
+//    limit: 10
+//  }).then(results => console.log(results));
+//}
+//render() {
+//  return (
+//    <div className="App">
+//      <Map/>
+//    </div>
+//  );
+//}
+//}
 
 export default App;
--- src/component/Map.js
+++ src/component/Map.js
@@ -4,10 +4,18 @@ import {withScriptjs,withGoogleMap,GoogleMap,Marker} from 'react-google-maps';
 
 const MyMapComponent = withScriptjs(
    withGoogleMap(props => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-      {props.isMarkerShown && (
-       <Marker position={{ lat: -34.397, lng: 150.644 }} />
-      )}
+    <GoogleMap
+      defaultZoom={8}
+      zoom={props.zoom}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      center={props.center}
+      >
+    {props.markers &&
+       props.markers
+         .filter(marker => marker.isVisible)
+         .map((marker, index) => (
+           <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+      ))}
       </GoogleMap>
     ))
   );
@@ -18,7 +26,8 @@ export default class Map extends Component {
   render() {
      return (
     <MyMapComponent
-      isMarkerShown
+    //  isMarkerShown
+      {...this.props}
       googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDvTdcm9JDj35YXqDbyWVBjDgDciYWuLPo"
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `400px` }} />}
