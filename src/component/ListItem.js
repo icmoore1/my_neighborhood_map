a39369b5a47dc6830a692c4460d730f7ad9bc504
Isaac Moore <icmoore1@bellsouth.net>
2019-03-14 11:58:56 -0400

    trying to find res error handling.

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -3,10 +3,9 @@ import React, { Component } from 'react';
 export default class ListItem extends Component {
   render() {
     return (
-      <li className="listItem" id= "listItem" onClick={() => this.props.handleListItemClick(this.props)}>
+      <li className="listItem" id= "listItem" tabIndex="0" aria-labelledby="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
        <img src={this.props.categories[0].icon.prefix + "32" + this.props.categories[0].icon.suffix}
        alt={this.props.categories[0].name}
-			 tabIndex="0"
        />
       {this.props.name}
     </li>
